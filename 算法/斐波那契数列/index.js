990f585ef892647e93958074a4d3fa69de98c40c
qhclf <3056149117@qq.com>
2019-11-02 10:05:56 +0800

    add new

--- 算法/斐波那契数列/index.js
+++ 算法/斐波那契数列/index.js
@@ -13,7 +13,7 @@ function getFibonacci(n){
     return fibarr;
 }
 
-console.log(getFibonacci(5));//[0, 1]
+console.log(getFibonacci(8));//[0, 1]
 
 //获取第n个斐波那契数字
 function getFibonacci_2(n){
@@ -23,3 +23,17 @@ function getFibonacci_2(n){
         return n;
     }
 }
+
+console.log(getFibonacci_2(6));//8
+
+function getFibonacci_3(n){
+    let num1 = 1, num2 = 1, num3 = 0;
+    for(let i=0;i< n-2; i++){
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    }
+    return num3;
+}
+
+console.log(getFibonacci_3(6))//8
---
+++ 算法/随机数/index.js
@@ -0,0 +1,5 @@
+function random(low, high){
+    return Math.floor(Math.random() * (high-low) + low);
+}
+
+console.log(random(1, 34));
